f17fb3654abcb213fa254a251a9ef3bd8da1b390
Darsh Patel <darshkpatel@gmail.com>
2019-03-23 17:56:34 +0530

    Added Sqlite Query time

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+import sqlite3
 app = Flask(__name__)
 from connection import redis
 from conf import TABLE_NAME
 import time
 
+conn = sqlite3.connect("data.sqlite")
+c = conn.cursor()
+
 CORS(app, resources=r"*")
+
+
 
 @app.route('/query', methods=["POST"])
 def query():
@@ -15,12 +20,17 @@ def query():
         print(query)
     else:
         return jsonify({"err": "Invalid parameters"})
-    start = time.time()
+    start_redis = time.time()
     redis.execute_command("REDISQL.EXEC", TABLE_NAME, query)
-    end = time.time() - start
+    end_redis = time.time() - start_redis
+
+    start_sqlite = time.time()
+    c.execute(str(query))
+    end_sqlite = time.time() - start_sqlite
 
     return jsonify({
-        "time_taken": end
+        "time_taken_redis": end_redis,
+        "time_taken_sqlite": end_sqlite
     })
 
 
